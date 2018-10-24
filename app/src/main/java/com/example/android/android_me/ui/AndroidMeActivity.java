3ce60102c62e8b83d9aa79ecb02d3374e3d9223e
James Kim <jhlink@users.noreply.github.com>
2018-10-24 10:23:56 -0400

    fix: Mark task[2-3] as DONE

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -28,9 +28,9 @@ public class AndroidMeActivity extends AppCompatActivity {
     // DONE (1) Create a layout file that displays one body part image named fragment_body_part.xml
         // This layout should contain a single ImageView
 
-    // TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
+    // DONE (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
         // In this class, you'll need to implement an empty constructor and the onCreateView method
-        // TODO (3) Show the first image in the list of head images
+        // DONE (3) Show the first image in the list of head images
             // Soon, you'll update this image display code to show any image you want
 
 
