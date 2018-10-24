e5710c3cb1b79a551447f69e2498ef73471bbf79
James Kim <jhlink@users.noreply.github.com>
2018-10-24 12:58:14 -0400

    task[4]: Add MasterListFragment to MainActivity fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MasterListFragment masterListFragment = new MasterListFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.main_activity_frag, masterListFragment)
+            .commit();
+    }
+}
